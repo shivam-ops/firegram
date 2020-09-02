b5c5285ca393abbc0a7a68ff7bcf2d54868bf570
Shivam Joshi <shivamjoshi0822@gmail.com>
2020-09-02 22:34:23 +0530

    Updates

---
+++ precache-manifest.e8d6cc0c5298914a8e7f222d16e9023d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a60f4039c73192ba31a236839bcadc1",
+    "url": "/picturegram/index.html"
+  },
+  {
+    "revision": "c58dd42c6daa355760ff",
+    "url": "/picturegram/static/css/main.65e427de.chunk.css"
+  },
+  {
+    "revision": "c04c622311411589a4bb",
+    "url": "/picturegram/static/js/2.6a546087.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/picturegram/static/js/2.6a546087.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c58dd42c6daa355760ff",
+    "url": "/picturegram/static/js/main.56bd38ee.chunk.js"
+  },
+  {
+    "revision": "c7d19291a5a044671a72",
+    "url": "/picturegram/static/js/runtime-main.5da2b0f1.js"
+  }
+]);
